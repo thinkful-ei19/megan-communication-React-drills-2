fe6b0544346e878c152ac8d4c49f742ca08c5b0d
Glitch (react-rate-calculator-starting-point) <none>
2018-04-10 13:33:57 -0500

    first commit

--- src/components/number-input.js
+++ src/components/number-input.js
@@ -4,7 +4,10 @@ export default function NumberInput(props) {
     return (
         <div className="form-group">
             <label htmlFor={props.id}>{props.label}</label>
-            <input type="number" id={props.id} min={props.min} max={props.max} />
+            <input type="number" id={props.id} min={props.min} max={props.max} 
+            value={props.value}
+            onChange={element=>props.onChange(element.target.value)} //what is e here?
+            />
         </div>
     );
 }
--- src/components/rate-calculator.js
+++ src/components/rate-calculator.js
@@ -6,14 +6,35 @@ import Output from './output';
 export default class RateCalculator extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state={
+            dayRate:300,
+            hours:5
+        }
+
+    }
+
+    updateDayRate(val){
+        this.setState({dayRate:val})
+    }
+
+    updateHours(val){
+        this.setState({hours:val})
     }
 
     render() {
-        const rate = 10;
+        const rate = this.state.dayRate/this.state.hours;
         return (
             <form>
-                <NumberInput id="day-rate" label="Day rate" min={0} max={5000} />
-                <NumberInput id="hours" label="Hours" min={1} max={12} />
+                <NumberInput id="day-rate" label="Day rate" min={0} max={5000}
+                    value={this.state.dayRate}
+                    onChange={val=> this.updateDayRate(val)}
+                />
+
+                <NumberInput id="hours" label="Hours" min={1} max={12} 
+                    value={this.state.hours}
+                    onChange={val=> this.updateHours(val)}
+                />
                 <Output id="hourly-rate" label="Hourly rate" value={rate.toFixed(2)}
 />
             </form>
